39c0a058f22a505219d7baa774352344dfe1f45f
Bartosz Chanowski <92587389+bchanowski@users.noreply.github.com>
2022-10-06 14:33:14 +0200

    Added needed imports urllib.parse(to tmeapi.py) and urllib.request(to readme example) (#2)
    * Added needed imports urllib.parse and urllib.request(to readme example)
    
    * Fixed space in symbols issue
    
    * Changed replace to quote_via
    
    Co-authored-by: Bartosz Chanowski <b.chanowski@tme.pl>

--- tmeapi.py
+++ tmeapi.py
@@ -1,18 +1,19 @@
 import urllib.request
+import urllib.parse
 import collections
 import base64
 import hmac
 import hashlib
 
 class Client():
-    def __init__(self, api_token, api_secret, api_host = 'https://api.tme.eu'):
+    def __init__(self, api_token, api_secret, api_host='https://api.tme.eu'):
         self.__api_token = api_token
         self.__api_secret = api_secret.encode()
         self.__api_host = api_host
 
     def __get_signature_base(self, url, params):
         params = collections.OrderedDict(sorted(params.items()))
-        encoded_params = urllib.parse.urlencode(params)
+        encoded_params = urllib.parse.urlencode(params, quote_via=urllib.parse.quote)
         signature_base = 'POST' + '&' + urllib.parse.quote(url, '') + '&' + urllib.parse.quote(encoded_params, '')
         return signature_base.encode()
 
@@ -20,7 +21,7 @@ class Client():
         hmac_value = hmac.new(self.__api_secret, self.__get_signature_base(url, params), hashlib.sha1).digest()
         return base64.encodebytes(hmac_value).rstrip()
 
-    def request(self, endpoint, params, format = 'json'):
+    def request(self, endpoint, params, format='json'):
         url = self.__api_host + endpoint + '.' + format
         params['Token'] = self.__api_token
         params['ApiSignature'] = self.__calculate_signature(url, params)
